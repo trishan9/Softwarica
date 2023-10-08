17c01cfcd013d355fd899d1afd55d50d80fa485a
Trishan Wagle <mailtotrishan@gmail.com>
2023-10-08 14:56:20 +0545

    feat: add oct2 and oct5 programming notes

---
+++ oct2.py
@@ -0,0 +1,153 @@
+# Immutable but also different memory location
+# Frozen Set
+# Byte
+# Range
+
+# Byte and Bytearray (0-255)
+
+# Complex Number - 2 + 3j - If printed, comes in float, Imaginary(int/float) number is mandatory
+a = 2 + 3j
+print(a.imag)
+
+
+# Task
+#Camel Case
+#string
+firstName = "Trishan"
+lastName = "Trishan"
+print(id(firstName))
+print(id(lastName))
+print(type(firstName), type(lastName))
+print(firstName is lastName)
+
+#integer
+firstNumber = 9
+lastNumber = 9
+print(id(firstNumber))
+print(id(lastNumber))
+print(type(firstNumber), type(lastNumber))
+print(firstNumber is lastNumber)
+
+
+#Pascal Case
+#string
+FirstName = "Trishan"
+LastName = "Trishan"
+print(id(FirstName))
+print(id(LastName))
+print(type(FirstName), type(LastName))
+print(FirstName is LastName)
+
+#integer
+FirstNumber = 9
+LastNumber = 9
+print(id(FirstNumber))
+print(id(LastNumber))
+print(type(FirstNumber), type(LastNumber))
+print(FirstNumber is LastNumber)
+
+
+#Snake Case
+#string
+first_name = "Trishan"
+last_name = "Trishan"
+print(id(first_name))
+print(id(last_name))
+print(type(first_name), type(last_name))
+print(first_name is last_name)
+
+#integer
+first_number = 9
+last_number = 9
+print(id(first_number))
+print(id(last_number))
+print(type(first_number), type(last_number))
+print(first_number is last_number)
+
+
+#Camel Snake Case
+#string
+first_Name = "Trishan"
+last_Name = "Trishan"
+print(id(first_Name))
+print(id(last_Name))
+print(type(first_Name), type(last_Name))
+print(first_Name is last_Name)
+
+#integer
+first_Number = 9
+last_Number = 9
+print(id(first_Number))
+print(id(last_Number))
+print(type(first_Number), type(last_Number))
+print(first_Number is last_Number)
+
+
+#Pascal Snake Case
+#string
+First_Name = "Trishan"
+Last_Name = "Trishan"
+print(id(First_Name))
+print(id(Last_Name))
+print(type(First_Name), type(Last_Name))
+print(First_Name is Last_Name)
+
+#integer
+First_Number = 9
+Last_Number = 9
+print(id(First_Number))
+print(id(Last_Number))
+print(type(First_Number), type(Last_Number))
+print(First_Number is Last_Number)
+
+
+#Flat Case
+#string
+firstname = "Trishan"
+lastname = "Trishan"
+print(id(firstname))
+print(id(lastname))
+print(type(firstname), type(lastname))
+print(firstname is lastname)
+
+#integer
+firstnumber = 9
+lastnumber = 9
+print(id(firstnumber))
+print(id(lastnumber))
+print(type(firstnumber), type(lastnumber))
+print(firstnumber is lastnumber)
+
+#Upper Flat Case
+#string
+FIRSTNAME = "Trishan"
+LASTNAME = "Trishan"
+print(id(FIRSTNAME))
+print(id(LASTNAME))
+print(type(FIRSTNAME), type(LASTNAME))
+print(FIRSTNAME is LASTNAME)
+
+#integer
+FIRSTNUMBER = 9
+LASTNUMBER = 9
+print(id(FIRSTNUMBER))
+print(id(LASTNUMBER))
+print(type(FIRSTNUMBER), type(LASTNUMBER))
+print(FIRSTNUMBER is LASTNUMBER)\
+
+#Sneaming Snake Case
+#string
+FIRST_NAME = "Trishan"
+LAST_NAME = "Trishan"
+print(id(FIRST_NAME))
+print(id(LAST_NAME))
+print(type(FIRST_NAME), type(LAST_NAME))
+print(FIRST_NAME is LAST_NAME)
+
+#integer
+FIRST_NUMBER = 9
+LAST_NUMBER = 9
+print(id(FIRST_NUMBER))
+print(id(LAST_NUMBER))
+print(type(FIRST_NUMBER), type(LAST_NUMBER))
+print(FIRST_NUMBER is LAST_NUMBER)
---
+++ oct5.py
@@ -0,0 +1,74 @@
+# Garbage Collection
+
+# Stack vs Private Heap Memory
+# Stack - Variables/Identifiers
+# Private Heap memory - Values/Objects
+
+
+# Strings, and String Manipulation
+print("Python\tWorld".expandtabs(12))
+print("Python\tWorld")
+print("Python\nWorld")
+print("Python\aWorld") # beep code
+txt = "Hello\rWorl" # Carriage Return
+print(txt) 
+print("Hello\0World")
+print(r"Hello\tWorld") # raw string: nullifies escape sequence characters
+print("C:\\Hello")
+
+a = "Hello"
+b = a.replace("H", "h")
+print(b)
+c = a.maketrans("Ho", "hO")
+print(a.translate(c))
+
+# Get el only from Hello using Slicing
+print(a[1::2])
+
+# Task
+'''
+Input: hello
+Output: wnhwnewnlwnlwno (add wn before each character)
+'''
+
+#First Method
+message = "hello"
+output = f"wn{message[0]}wn{message[1]}wn{message[2]}wn{message[3]}wn{message[4]}"
+print(output)
+
+#Another Method
+output = message.replace("h", "wnh").replace("e", "wne").replace("l", "wnl").replace("o", "wno")
+print(output)
+
+#Another Method
+output = message.replace("hello", "wnhwnewnlwnlwno")
+print(output)
+
+#Another Method
+final = ""
+for char in message:
+    output = "wn"+char
+    final += output
+print(final)
+
+#Another Method
+output_list = list(map(lambda char: 'wn'+char, message))
+print("".join(output_list))
+
+#Another Method
+class StringManipulation:
+    '''This class contains methods for string manipulation''' #Doc-Strings
+    def __init__(self, string):
+        self._string = string
+
+    def replace_string(self):
+        '''returns string by adding "wn" before each character'''
+        output_list = list(map(lambda char: 'wn'+char, self._string))
+        output = "".join(output_list)
+        return output
+    
+print(StringManipulation.__doc__)
+new_string = StringManipulation("hello")
+print(new_string.replace_string())
+
+# Indentation in Python - Important
